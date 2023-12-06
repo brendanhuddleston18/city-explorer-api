47df7e35b8ef04095c0afd87ba92ea44be6a643f
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-05 23:23:19 -0600

    added feature to send weather and city to client

--- server.js
+++ server.js
@@ -8,19 +8,35 @@ const app = express();
 
 app.use(cors());
 
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
+    this.description = description;
+  }
+}
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   console.log(request.query);
   // chatGPT helped with this
   let userLat = request.query.latitude;
   let userLon = request.query.longitude;
 
-  if (userLat && userLat) {
-    //
-    let userCity = weatherData.find((city)=> weatherData.lon === userLon && weatherData.lat === weatherData.lon );
-    console.log(userCity);
-    response.send(`your city is ${userCity}`);
+  if (userLat && userLon) {
+    let userCity = weatherData.find(
+      (city) => city.lon === userLon && city.lat === userLat
+    );
+    if (userCity) {
+      console.log(userCity.data);
+
+      const forecastArray = userCity.data.map((day) => {
+        const date = day.valid_date;
+        const description = day.weather.description;
+        return new Forecast(date, description);
+      });
+      response.json({ "CityName": userCity.city_name, "forecast": forecastArray });
+    }
   } else {
     console.log("didn't work bro");
   }
