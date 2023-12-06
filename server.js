07d2386b8774b3c09770686bf39d9b58a61937f6
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-05 20:52:38 -0600

    added weatherdata.find

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const weatherData = require('./data/weather.json');
+require("dotenv").config();
+const weatherData = require("./data/weather.json");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/weather', (request, response) => {
+app.get('/', (request, response) => {
+  console.log(request.query);
   // chatGPT helped with this
-let userLat = request.query.latitude;
-let userLon = request.query.longitude;
+  let userLat = request.query.latitude;
+  let userLon = request.query.longitude;
 
-  
-  
-  if(userLat && userLat){
-    // 
-    // response.json(weatherData[cityData]);
-    response.send(`your longitude is ${userLon} and latitude is ${userLat}`);
-  }else {
+  if (userLat && userLat) {
+    //
+    let userCity = weatherData.find((city)=> weatherData.lon === userLon && weatherData.lat === weatherData.lon );
+    console.log(userCity);
+    response.send(`your city is ${userCity}`);
+  } else {
     console.log("didn't work bro");
   }
-})
+});
 
 // app.get('/', (request, response) => {
 //   response.send("Hello World");
