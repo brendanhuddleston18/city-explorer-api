1f3d97629b569b985059db886e8e06ed3db380b9
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-07 12:55:13 -0600

    cleaned code

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ class Forecast {
 }
 
 class Movie {
-  constructor(name, description, voteAvg){
+  constructor(name, description, voteAvg) {
     this.name = name;
     this.description = description;
     this.voteAvg = voteAvg;
@@ -30,61 +30,57 @@ class Movie {
 }
 
 app.get("/", async (request, response) => {
-  // console.log(request.query);
   // chatGPT helped with this
   let userLat = request.query.latitude;
   let userLon = request.query.longitude;
   let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_KEY}&lat=${userLat}&lon=${userLon}&units=I&days=7`;
-  // http://api.weatherbit.io/v2.0/forecast/daily?key=86ce6a03e5ab4ffdaaf5b467eb10fb9c&lat=47.603832&lon=-122.330062&units=I&days=16
-  let weatherResponse = await axios.get(weatherURL);
 
-  // console.log(weatherResponse.data);
-  if (weatherResponse) {
+  try {
+    let weatherResponse = await axios.get(weatherURL);
     const forecastArray = weatherResponse.data.data.map((day) => {
       const date = day.datetime;
       const highTemp = day.high_temp;
       const lowTemp = day.low_temp;
       return new Forecast(date, highTemp, lowTemp);
     });
+
     response.json({ Forecast: forecastArray });
+  } catch (error) {
+    let errorMessage = error.message;
+    console.error(errorMessage);
   }
-
 });
-//   if (userLat && userLon) {
 
-app.get('/movies', async (request, response) => {
+app.get("/movies", async (request, response) => {
   let city = request.query.city;
   console.log(city);
-  let movieURL = "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
-  if(city){
+  let movieURL =
+    "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+  if (city) {
     let movieResponse = await axios.get(movieURL, {
-      params: {query: `${city}`},
+      params: { query: `${city}` },
       headers: {
         accept: "application/json",
         Authorization: `Bearer ${MOVIE_READ}`,
       },
     });
-    if(movieResponse) {
-      const movieArray = movieResponse.data.results.sort((a,b)=> b.vote_average - a.vote_average);
+    if (movieResponse) {
+      const movieArray = movieResponse.data.results.sort(
+        (a, b) => b.vote_average - a.vote_average
+      );
       console.log(movieArray);
-      const sortedMovies = movieArray.map(value => {
+      const sortedMovies = movieArray.map((value) => {
         const name = value.original_title;
         const description = value.overview;
         const voteAvg = value.vote_average;
         return new Movie(name, description, voteAvg);
-
       });
-      //  .map((movie)=> {
-      
-      // })
-      // console.log(movieResponse);
+
       response.json(sortedMovies);
     } else {
       console.log("No movies found");
     }
-
   }
 });
-
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
