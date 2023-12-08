5fbcbc68b980605f68d7f6e9182d7a563fb73cec
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-12-08 17:51:31 -0600

    added caches for both movies and weather

--- handlers/movies.js
+++ handlers/movies.js
@@ -12,36 +12,44 @@ class Movie {
   }
 }
 
+const movieCache = {};
+
 
 async function getMovies (request, response) {
   let city = request.query.city;
-  console.log(city);
-  let movieURL =
-    "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
-  if (city) {
-    let movieResponse = await axios.get(movieURL, {
-      params: { query: `${city}` },
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${MOVIE_READ}`,
-      },
-    });
-    if (movieResponse) {
-      const movieArray = movieResponse.data.results.sort(
-        (a, b) => b.vote_average - a.vote_average
-      );
-      console.log(movieArray);
-      const sortedMovies = movieArray.map((value) => {
-        const name = value.original_title;
-        const description = value.overview;
-        const voteAvg = value.vote_average;
-        return new Movie(name, description, voteAvg);
-      });
+  // console.log(city);
 
-      response.json(sortedMovies);
-    } else {
-      console.log("No movies found");
+  if (! movieCache[city]){
+    
+    let movieURL =
+      "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+    if (city) {
+      let movieResponse = await axios.get(movieURL, {
+        params: { query: `${city}` },
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${MOVIE_READ}`,
+        },
+      });
+      if (movieResponse) {
+        const movieArray = movieResponse.data.results.sort(
+          (a, b) => b.vote_average - a.vote_average
+        );
+        // console.log(movieArray);
+        const sortedMovies = movieArray.map((value) => {
+          const name = value.original_title;
+          const description = value.overview;
+          const voteAvg = value.vote_average;
+          return new Movie(name, description, voteAvg);
+        });
+        movieCache[city] = sortedMovies;
+        response.json(sortedMovies);
+      } else {
+        console.log("No movies found");
+      }
     }
+  } else {
+    console.log(`We have ${city} at home`);
   }
 };
 
--- handlers/weather.js
+++ handlers/weather.js
@@ -10,27 +10,35 @@ class Forecast {
     this.lowTemp = lowTemp;
   }
 }
+let weatherCache = {};
 
 async function getWeather(request, response) {
   // chatGPT helped with this
+  
   let userLat = request.query.latitude;
   let userLon = request.query.longitude;
   let weatherURL = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_KEY}&lat=${userLat}&lon=${userLon}&units=I&days=7`;
 
-  try {
-    let weatherResponse = await axios.get(weatherURL);
-    const forecastArray = weatherResponse.data.data.map((day) => {
-      const date = day.datetime;
-      const highTemp = day.high_temp;
-      const lowTemp = day.low_temp;
-      return new Forecast(date, highTemp, lowTemp);
-    });
-
-    response.json({ Forecast: forecastArray });
-  } catch (error) {
-    let errorMessage = error.message;
-    console.error(errorMessage);
+  if ( ! weatherCache[userLat, userLon]){
+    
+    try {
+      console.log(`getting weather results for ${userLat} ${userLon}`);
+      let weatherResponse = await axios.get(weatherURL);
+      const forecastArray = weatherResponse.data.data.map((day) => {
+        const date = day.datetime;
+        const highTemp = day.high_temp;
+        const lowTemp = day.low_temp;
+        return new Forecast(date, highTemp, lowTemp);
+      });
+      weatherCache[userLat, userLon] = forecastArray;
+  
+      response.json({ Forecast: forecastArray });
+    } catch (error) {
+      let errorMessage = error.message;
+      console.error(errorMessage);
+    }
+  } else {
+    console.log(`we have ${userLat} ${userLon} at home.`);
   }
 };
-
 module.exports = getWeather;
